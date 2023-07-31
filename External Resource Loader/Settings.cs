eb3647824f0670bdaf2e66a584893b8d3fff47e5
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2023-07-31 19:32:00 +0300

    Update Settings.cs

--- External Resource Loader/Settings.cs
+++ External Resource Loader/Settings.cs
@@ -5,10 +5,9 @@ namespace ExternalResourceLoader
 {
     public static class Settings
     {
-        public static string DataFolderName = "Data";
-        public static string ResourcesFolderName = "ExternalResources";
-        public static string ResourcesPath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, ResourcesFolderName);
-        public static string BuildPath = Path.Combine(ResourcesPath, DataFolderName);
-        public static string LoadPath = Path.Combine(ResourcesPath, DataFolderName);
+        public static string DataFolderName => "Data";
+        public static string ResourcesPath => Path.Combine(Directory.GetParent(Application.dataPath).FullName, "ExternalResources");
+        public static string BuildPath => Path.Combine(ResourcesPath, DataFolderName);
+        public static string LoadPath { get; set; } = Path.Combine(ResourcesPath, DataFolderName);
     }
 }
